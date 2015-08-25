a9ba03697a3feb3725dacf01d3db3b23fd311332
Doug Davis <dug@us.ibm.com>
2015-08-25 12:26:53 -0700

    PoC
    Signed-off-by: Doug Davis <dug@us.ibm.com>

---
+++ api/server/swagger/server/server.go
@@ -0,0 +1,32 @@
+package swserver
+
+import (
+	"net/http"
+
+	"github.com/emicklei/go-restful"
+	"github.com/emicklei/go-restful/swagger"
+	//"github.com/icecrime/docker-api/api"
+	api "github.com/docker/docker/api/server/swagger/api"
+)
+
+// New creates a new Docker remote API service instance in the form of an
+// http.Handler.
+func New(impl api.Service, swaggerFilePath string) http.Handler {
+	// Need to check the types on these so we don't panic
+	baseSrv := newBaseServer(impl)
+	containersSrv := newContainersServer(impl)
+
+	container := restful.NewContainer()
+	container.Add(baseSrv.WebService)
+	container.Add(containersSrv.WebService)
+
+	swaggerConf := swagger.Config{
+		WebServices:     container.RegisteredWebServices(),
+		ApiPath:         "/docs/apidocs.json",
+		SwaggerPath:     "/docs/swagger/",
+		SwaggerFilePath: swaggerFilePath,
+	}
+	swagger.RegisterSwaggerService(swaggerConf, container)
+
+	return container
+}
